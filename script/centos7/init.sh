8fe7060f076edd3bee7151f651868472838a16ec
YeLuo <yeluo0528@gmail.com>
2023-12-13 14:43:43 +0800

    Update Docker script and add CentOS initialization script

--- script/centos7/init.sh
+++ script/centos7/init.sh
@@ -4,13 +4,20 @@ set -e
 
 # Set timezone
 set_time_zone() {
-  echo -n 'Please enter the timezone (press enter to use the default timezone "Asia/Shanghai"):'
-  read -r timezone
-  timezone=${timezone:-"Asia/Shanghai"}
+  while true; do
+    echo -n 'Please enter the timezone (press enter to use the default timezone "Asia/Shanghai"): '
+    read -r timezone
+    timezone=${timezone:-"Asia/Shanghai"}
 
-  echo 'Setting timezone ......'
-  timedatectl set-timezone "$timezone"
-  echo 'Timezone is set to' "$timezone" '......'
+    if timedatectl list-timezones | grep -q "^$timezone$"; then
+      echo 'Setting timezone ......'
+      timedatectl set-timezone "$timezone"
+      echo 'Timezone is set to' "$timezone" '......'
+      break
+    else
+      echo "Invalid timezone: $timezone. Please try again."
+    fi
+  done
 }
 
 main(){
