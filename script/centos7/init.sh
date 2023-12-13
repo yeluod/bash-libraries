2f2834d74b2b510f4e2794cfc7d98702eaa44e47
YeLuo <yeluo0528@gmail.com>
2023-12-13 15:45:18 +0800

    Add scheduled time synchronization to init script

--- script/centos7/init.sh
+++ script/centos7/init.sh
@@ -2,46 +2,71 @@
 
 set -e
 
+log() {
+  local msg_type="$1"
+  local msg="$2"
+  case "$msg_type" in
+    info)
+      echo -e "\e[32m[INFO] $msg\e[0m"  # Green
+      ;;
+    warning)
+      echo -e "\e[33m[WARNING] $msg\e[0m"  # Yellow
+      ;;
+    error)
+      echo -e "\e[31m[ERROR] $msg\e[0m"  # Red
+      ;;
+    *)
+      echo "[UNKNOWN] $msg"
+      ;;
+  esac
+}
+
 # Set timezone
 set_time_zone() {
+  log "info" 'Starting to set timezone ......'
   while true; do
-    echo -n 'Please enter the timezone (press enter to use the default timezone "Asia/Shanghai"): '
+    log "info" 'Please enter the timezone (press enter to use the default timezone "Asia/Shanghai"): '
     read -r timezone
     timezone=${timezone:-"Asia/Shanghai"}
 
     if timedatectl list-timezones | grep -q "^${timezone}$"; then
-      echo 'Setting timezone ......'
+      log "info" 'Setting timezone ......'
       timedatectl set-timezone "${timezone}"
-      echo 'Timezone is set to' "${timezone}" '......'
+      log "info" 'Timezone is set to' "${timezone}" '......'
       break
     else
-      echo "Invalid timezone: ${timezone}. Please try again."
+      log "error" "Invalid timezone: ${timezone}. Please try again."
     fi
   done
 }
 
 # Set scheduled time synchronization
 set_scheduled_sync_time(){
-  echo -n 'Please enter the cron expression (press enter to use the default expression "0 */1 * * *"): '
+  log "info" 'Starting to set up time synchronization task ......'
+
+  log "info" 'Please enter the cron expression (press enter to use the default expression "0 */1 * * *"): '
   read -r cron_expr
   cron_expr=${cron_expr:-"0 */1 * * *"}
 
   while true; do
-    echo -n 'Please enter the NTP server (press enter to use the default server "ntp1.aliyun.com"): '
+    log "info" 'Please enter the NTP server (press enter to use the default server "ntp1.aliyun.com"): '
     read -r ntp_server
     ntp_server=${ntp_server:-"ntp1.aliyun.com"}
 
     if ping -c 1 "${ntp_server}" &> /dev/null; then
-      echo "${ntp_server} is reachable. Setting up scheduled time synchronization ..."
+      log "info" "${ntp_server} is reachable. Setting up scheduled time synchronization ..."
       break
     else
-      echo "Cannot reach ${ntp_server}. Please try again."
+      log "error" "Cannot reach ${ntp_server}. Please try again."
     fi
   done
 
   yum -y install ntp ntpdate crontab
 
+  log "info" 'Starting to synchronize time ......'
   /usr/sbin/ntpdate "${ntp_server}"
+
+  log "info" 'Writing time synchronization task ......'
   (crontab -l ; echo "${cron_expr} /usr/sbin/ntpdate ${ntp_server}") | crontab -
   systemctl restart crond && systemctl enable crond --now
   systemctl status crond
